41c163f076c58306103633164cb86108c6bd39b5
Victor Garcia Zarco <victor.gzarco@gmail.com>
2015-01-23 18:53:41 +0100

    Añadida ruta y método de prueba en el controlador principal para probar a añadir nuevos usuarios de empresa. Probar con la ruta->  localhost:9000/newcompany/usuario/contraseña y comprobar en la base de datos (adeccotest) que los datos se guardan

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,21 @@
 package controllers;
 
+import models.datasource.CompanyUserDataSource;
+import models.entities.CompanyUser;
 import play.*;
 import play.mvc.*;
-
 import views.html.*;
 
 public class Application extends Controller {
 
     public static Result index() {
         return TODO;
+    }
+    
+    public static Result newUser(String email, String password){
+    	CompanyUser companyUser = new CompanyUser(email, password);
+    	CompanyUserDataSource.insertIntoCompanyUser(companyUser);
+    	return ok("Usuario: "+email + " con contraseña: "+password+" añadido.");
     }
 
 }
