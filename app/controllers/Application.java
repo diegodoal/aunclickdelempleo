3799213da7a3d65c180f0cd79089ee184ec3e413
Victor Garcia Zarco <victor.gzarco@gmail.com>
2015-01-27 19:27:58 +0100

    Implementado envío de email personalizado para validar tu cuenta de usuario particular. La url ya contiene la dirección completa desde la que validar

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -17,7 +17,6 @@ public class Application extends Controller {
 		return TODO;
 	}
 
-
 	/* EMAIL
 	 * Outgoing Mail (SMTP) Server
 	 * requires TLS or SSL: smtp.gmail.com (use authentication)
@@ -44,8 +43,50 @@ public class Application extends Controller {
 		};
 		Session session = Session.getInstance(props, auth);
 
-		// Cambiar el mensaje del título del mail y el cuerpo del mensaje
+		// Cambiar el mensaje del título del mail y el cuerpo del mensaje		
 		EmailUtil.sendEmail(session, toEmail,"Confirmación de cuenta creada en www.aunclickdelempleo.com", "Su cuenta " + toEmail + " se ha creado con éxito");
 		return ok("Se ha enviado un email a: " + toEmail);
-	}   
+	} 
+	
+	/* EMAIL
+	 * Outgoing Mail (SMTP) Server
+	 * requires TLS or SSL: smtp.gmail.com (use authentication)
+	 * Use Authentication: Yes
+	 * Port for TLS/STARTTLS: 587
+	 */
+	public static Result sendValidationAccountEmail(String toEmail){
+		final String fromEmail = "aunclickdelempleo@gmail.com"; //requires valid gmail id
+		final String password = "webadecco"; // correct password for gmail id
+		// Por ejemplo poner toEmail = "useradecco@gmail.com" // can be any email id 
+
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "smtp.gmail.com"); //SMTP Host
+		props.put("mail.smtp.port", "587"); //TLS Port
+		props.put("mail.smtp.auth", "true"); //enable authentication
+		props.put("mail.smtp.starttls.enable", "true"); //enable STARTTLS
+
+		//create Authenticator object to pass in Session.getInstance argument
+		Authenticator auth = new Authenticator() {
+			//override the getPasswordAuthentication method
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(fromEmail, password);
+			}
+		};
+		Session session = Session.getInstance(props, auth);
+
+		// Cambiar el mensaje del título del mail y el cuerpo del mensaje
+		String message = "Para activar tu cuenta, haz click en el siguiente enlace: http://localhost:9000/particular/verify";
+		
+		ParticularUser query = ParticularUserDataSource.getParticularUser(toEmail);
+		if(query == null){
+			return badRequest("Error, no se ha podido encontrar el usuario");
+		}else if(query.emailVerificationKey == null){
+			return badRequest("El usuario ya ha sido validado, no es necesario enviar este mensaje");
+		}else if(query != null){
+			message += "/"+query.email+"/"+query.emailVerificationKey;
+		}
+		
+		EmailUtil.sendEmail(session, toEmail,"Confirmación de cuenta creada en www.aunclickdelempleo.com", message);
+		return ok("Se ha enviado un email a: " + toEmail);
+	}  
 }
