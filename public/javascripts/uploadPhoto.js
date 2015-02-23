90d5c3c2c1f5e3494a659ac7e26e0fafa67a4611
Victor Garcia Zarco <victor.gzarco@gmail.com>
2015-02-23 17:46:07 +0100

    Añadido script para gestionar las cargas de archivos, y refactor en la vista photo.scala.html

--- public/javascripts/uploadPhoto.js
+++ public/javascripts/uploadPhoto.js
@@ -1,4 +1,16 @@
 function uploadPhoto(){
+var input = document.getElementById('input');
+input.addEventListener('change', handleFiles);
+
+function handleFiles(e) {
+    var ctx = document.getElementById('canvas').getContext('2d');
+    var img = new Image;
+    img.src = URL.createObjectURL(e.target.files[0]);
+    img.onload = function() {
+        ctx.drawImage(img, 20,20);
+        alert('the image is drawn');
+    }
+}
 }
 
 function takePhotoModal(){
