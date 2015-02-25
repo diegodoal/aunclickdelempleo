987ad98d9c9a2d4c311d136b9d84f00504be836c
Victor Garcia Zarco <victor.gzarco@gmail.com>
2015-02-25 18:39:53 +0100

    Javascript: dibujar canvas en el modal de hacer foto

--- public/javascripts/photo.js
+++ public/javascripts/photo.js
@@ -16,3 +16,55 @@ function drawFromInput(){
         }
     }
 }
+
+function takePhoto(){
+  var streaming = false,
+      video        = document.querySelector('#video'),
+      canvasModal  = document.querySelector('#canvasModal'),
+      canvasMain   = document.querySelector('#mainCanvas'),
+      takePhotoButton  = document.querySelector('#takePhotoButton'),
+      acceptPhotoButton = document.querySelector('#acceptPhotoButton'),
+      width = CANVAS_WIDTH,
+      height = 0;
+
+  navigator.getMedia = ( navigator.getUserMedia ||
+                     navigator.webkitGetUserMedia ||
+                     navigator.mozGetUserMedia ||
+                     navigator.msGetUserMedia);
+
+  navigator.getMedia(
+    {video: true, audio: false},
+    function(stream) {
+        if (navigator.mozGetUserMedia) {
+            video.mozSrcObject = stream;
+        } else {
+            var vendorURL = window.URL || window.webkitURL;
+            video.src = vendorURL ? vendorURL.createObjectURL(stream) : stream;
+        }
+        video.play();
+    },
+    function(err) {
+        console.log("An error occured! " + err);
+    }
+  );
+
+  video.addEventListener('canplay', function(ev){
+      if (!streaming) {
+      CANVAS_HEIGHT = video.videoHeight / (video.videoWidth/CANVAS_WIDTH);
+      video.setAttribute('width', CANVAS_WIDTH);
+      video.setAttribute('height', CANVAS_HEIGHT);
+      streaming = true;
+      }
+      }, false
+  );
+
+  takePhotoButton.addEventListener('click', function(ev){
+      canvasModal.width = CANVAS_WIDTH;
+      canvasModal.height = CANVAS_HEIGHT;
+      canvasModal.getContext('2d').drawImage(video, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+      ev.preventDefault();
+      }, false
+  );
+
+
+}
