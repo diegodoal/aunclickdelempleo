ed04965660485b8a46cc38d0b509877b6a2b4a1f
Victor Garcia Zarco <victor.gzarco@gmail.com>
2015-03-02 16:31:23 +0100

    Añadido drag and drop a la vista photo

--- public/javascripts/photo.js
+++ public/javascripts/photo.js
@@ -103,3 +103,43 @@ function finishPhoto(){
           window.location.assign("/orientation")
         });
 }
+
+function dragPhoto() {
+    var canvas = document.querySelector('#mainCanvas'),
+        container = document.getElementById("drag-drop-input"),
+        context = canvas.getContext("2d"),
+        img = document.createElement("img"),
+        clearCanvas = function () {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+        };
+        
+    
+    // Image for loading    
+    img.addEventListener("load", function () {
+        clearCanvas();
+        context.drawImage(img, 0, 0, 200, 200);
+    }, false);
+
+
+    // To enable drag and drop
+    container.addEventListener("dragover", function (evt) {
+        evt.preventDefault();
+    }, false);
+
+    // Handle dropped image file - only Firefox and Google Chrome
+    container.addEventListener("drop", function (evt) {
+        var files = evt.dataTransfer.files;
+        if (files.length > 0) {
+            var file = files[0];
+            if (typeof FileReader !== "undefined" && file.type.indexOf("image") != -1) {
+                var reader = new FileReader();
+                // Note: addEventListener doesn't work in Google Chrome for this event
+                reader.onload = function (evt) {
+                    img.src = evt.target.result;
+                };
+                reader.readAsDataURL(file);
+            }
+        }
+        evt.preventDefault();
+    }, false);   
+}
