47c3ce88fccc6cf3576f79a3004bfd5888ba8577
Victor Garcia Zarco <victor.gzarco@gmail.com>
2015-03-05 20:11:39 +0100

    Sacado script a un fichero aparte

---
+++ public/javascripts/speech-recognition.js
@@ -0,0 +1,42 @@
+function loadRecognition(){
+// Test browser support
+      window.SpeechRecognition = window.SpeechRecognition       ||
+                                 window.webkitSpeechRecognition ||
+                                 null;
+
+      //If not supported, hide button
+      if(window.SpeechRecognition === null) {
+        document.getElementById('start-speech-btn').setAttribute('class', 'hidden');
+      }else{
+        var recognizer = new window.SpeechRecognition();
+        var input_name;
+
+        recognizer.continuous = false;
+        recognizer.lang = 'es-ES';
+        recognizer.interimResults = false; //Constantly refreshes text while recognition is active
+
+        $('.input-speech button').click(function() {
+          //alert($(this).attr('id'));
+          input_name = $(this).parent().children('input');
+          //alert(input_name.val());
+          try{
+            recognizer.start();
+          }catch(ex){
+            alert("Error en el reconocimiento de voz: "+ex.value);
+          }
+        });
+
+        recognizer.onresult = function(event){
+          input_name.value = '';
+          for(var i = event.resultIndex; i < event.results.length; i++) {
+            if(event.results[i].isFinal) {
+              input_name.val(event.results[i][0].transcript);
+            }
+          }
+        };
+
+        recognizer.onerror = function(event) {
+          alert("Error en reconocimiento. OnError: "+event.message);
+        };
+      }
+}
