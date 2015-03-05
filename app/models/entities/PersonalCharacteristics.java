cf645bb6dd68f234d8af9068205c5a7a4ccb4cc2
Victor Garcia Zarco <victor.gzarco@gmail.com>
2015-03-05 16:48:40 +0100

    Entidad de Características Personales (Personal Characteristics)

---
+++ app/models/entities/PersonalCharacteristics.java
@@ -0,0 +1,15 @@
+package models.entities;
+
+import java.util.List;
+
+/**
+ * Created by Victor on 05/03/2015.
+ */
+public class PersonalCharacteristics {
+
+    public List<String> personalCharacteristicsList;
+
+    public PersonalCharacteristics(List<String> personalCharacteristicsList){
+        this.personalCharacteristicsList = personalCharacteristicsList;
+    }
+}
