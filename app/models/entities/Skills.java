6862b1a2df6f98db9d8d9dbb400068c625631b9c
Victor Garcia Zarco <victor.gzarco@gmail.com>
2015-03-05 16:43:21 +0100

    Entidad Habilidades (Skills)

---
+++ app/models/entities/Skills.java
@@ -0,0 +1,27 @@
+package models.entities;
+
+import java.util.List;
+
+/**
+ * Created by Victor on 05/03/2015.
+ */
+public class Skills {
+
+    public List<Skill> skillList;
+
+    public Skills(List<Skill> skillList){
+        this.skillList = skillList;
+    }
+
+    public class Skill{
+
+        // Level must be on of: "Normal, Bien, Excelente"
+        public String name;
+        public String level;
+
+        public Skill(String name, String level){
+            this.name = name;
+            this.level = level;
+        }
+    }
+}
