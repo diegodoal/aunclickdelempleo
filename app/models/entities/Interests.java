b1633d106d98a5c81ea18379d649b017deabb285
Victor Garcia Zarco <victor.gzarco@gmail.com>
2015-03-05 16:46:28 +0100

    Entidad de Intereses (Interests)

---
+++ app/models/entities/Interests.java
@@ -0,0 +1,15 @@
+package models.entities;
+
+import java.util.List;
+
+/**
+ * Created by Victor on 05/03/2015.
+ */
+public class Interests {
+
+    List<String> interestsList;
+
+    public Interests(List<String> interestsList){
+        this.interestsList = interestsList;
+    }
+}
