0573e66617bbc633e212e5e14fd90ed5dad8f281
Victor Garcia Zarco <victor.gzarco@gmail.com>
2015-03-17 17:20:07 +0100

    Limpieza en el proyecto. - Eliminados controladores de Login Particular y Login Company. Ahora sólo hay un LoginController. - Eliminadas entidades y DataSource de ParticularUser y CompanyUser. - Adaptado el menú de navegación al nuevo diseño.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,9 +1,7 @@
 package controllers;
 
-import models.datasource.CompanyUserDataSource;
-import models.datasource.ParticularUserDataSource;
-import models.entities.CompanyUser;
-import models.entities.ParticularUser;
+import models.datasource.UserDataSource;
+import models.entities.User;
 import play.mvc.*;
 import utils.EmailUtil;
 import java.util.Properties;
@@ -81,7 +79,7 @@ public class Application extends Controller {
 		// Cambiar el mensaje del título del mail y el cuerpo del mensaje
 		String message = "Para activar tu cuenta, haz click en el siguiente enlace: http://localhost:9000/particular/verify";
 		
-		ParticularUser query = ParticularUserDataSource.getParticularUser(toEmail);
+		User query = UserDataSource.getUserByEmail(toEmail);
 		if(query == null){
 			return badRequest("Error, no se ha podido encontrar el usuario");
 		}else if(query.emailVerificationKey == null){
@@ -93,45 +91,5 @@ public class Application extends Controller {
 		EmailUtil.sendEmail(session, toEmail,"Confirmación de cuenta creada en www.aunclickdelempleo.com", message);
 		return ok("Se ha enviado un email a: " + toEmail+" .Por favor, revise su correo");
 	}  
-	
-	/**
-	 * This method sends an email to the new registered COMPANY USER with the URL for validate its account
-	 * @param toEmail Email of the new registered user
-	 * @return A controller action
-	 */
-	public static Result sendCompanyValidationEmail(String toEmail){
-		final String fromEmail = "aunclickdelempleo@gmail.com"; //requires valid gmail id
-		final String password = "webadecco"; // correct password for gmail id
-		// Por ejemplo poner toEmail = "useradecco@gmail.com" // can be any email id 
 
-		Properties props = new Properties();
-		props.put("mail.smtp.host", "smtp.gmail.com"); //SMTP Host
-		props.put("mail.smtp.port", "587"); //TLS Port
-		props.put("mail.smtp.auth", "true"); //enable authentication
-		props.put("mail.smtp.starttls.enable", "true"); //enable STARTTLS
-
-		//create Authenticator object to pass in Session.getInstance argument
-		Authenticator auth = new Authenticator() {
-			//override the getPasswordAuthentication method
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(fromEmail, password);
-			}
-		};
-		Session session = Session.getInstance(props, auth);
-
-		// Cambiar el mensaje del título del mail y el cuerpo del mensaje
-		String message = "Para activar tu cuenta, haz click en el siguiente enlace: http://localhost:9000/company/verify";
-		
-		CompanyUser query = CompanyUserDataSource.getCompanyUser(toEmail);
-		if(query == null){
-			return badRequest("Error, no se ha podido encontrar el usuario");
-		}else if(query.emailVerificationKey == null){
-			return badRequest("El usuario ya ha sido validado, no es necesario enviar este mensaje");
-		}else if(query != null){
-			message += "/"+query.email+"/"+query.emailVerificationKey;
-		}
-		
-		EmailUtil.sendEmail(session, toEmail,"Confirmación de cuenta creada en www.aunclickdelempleo.com", message);
-		return ok("Se ha enviado un email a: " + toEmail+" .Por favor, revise su correo");
-	}
 }
