c5cbd7bdce5e44747e6d0818796b867ee14605fd
Mikel G. N <mikel.garcia.najera@gmail.com>
2015-03-31 11:41:44 +0200

    Arreglado script de añadir experiencia, antes si tenías varios añadidos y le dabas a eliminar uno de ellos se quitaban todos.

--- public/javascripts/add-new.js
+++ public/javascripts/add-new.js
@@ -2,7 +2,7 @@ function addNew(){
     $('body').on( "click", ".add-new", function() {
         //Removing new section
         if($(this).attr('src') == '/assets/images/orientation/ic_trash2.png'){
-            $(this).parent().parent().parent().parent().parent().remove();
+            $(this).parent().parent().parent().parent().remove();
         }else{ //Adding new section
             var clone = $(this).parent().parent().parent().parent().parent().clone();
             clone.find('.form-control').each(function(){
