da202e9a237cdc7a12ca973e0a2f48bae2108255
Victor Garcia Zarco <victor.gzarco@gmail.com>
2015-04-08 18:36:30 +0200

    Añadido mostrar información básica en texto plano al pulsar sobre tu nombre de usuario

---
+++ app/controllers/ProfileController.java
@@ -0,0 +1,20 @@
+package controllers;
+
+import models.datasource.SingletonDataSource;
+import models.entities.User;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+/**
+ * Created by Victor on 08/04/2015.
+ */
+public class ProfileController extends Controller {
+
+    public static Result blank(){
+        User user = SingletonDataSource.getInstance().getUserByEmail(session().get("email"));
+        if (user == null){
+            return redirect("/");
+        }
+        return ok(views.html.user_profile.user_info.render(user));
+    }
+}
