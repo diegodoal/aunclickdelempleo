3efe8a22dfb932d918d330f468b4d02a8721963d
Victor Garcia Zarco <victor.gzarco@gmail.com>
2015-04-09 18:25:00 +0200

    Sobreescrito método .equals() en InterviewSchedule. Ahora ya no se guardan repetidas las entrevistas si ya hay una igual en el perfil del usuario

--- app/controllers/OrientationController.java
+++ app/controllers/OrientationController.java
@@ -253,8 +253,9 @@ public class OrientationController extends Controller {
 
         String[][] result = new Gson().fromJson(request.toString(), new TypeToken<String[][]>() {}.getType());
         for(int i=0; i<result.length; i++){
-            if(!user.interviewScheduleList.contains(result[i])){
-                user.interviewScheduleList.add(new InterviewSchedule(result[i][0] + " " + result[i][1], result[i][2], result[i][3]));
+            InterviewSchedule auxInterviewSchedule = new InterviewSchedule(result[i][0] + " " + result[i][1], result[i][2], result[i][3]);
+            if(!user.interviewScheduleList.contains(auxInterviewSchedule)){
+                user.interviewScheduleList.add(auxInterviewSchedule);
             }
         }
 
