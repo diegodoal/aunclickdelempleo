b710f6693b79fcf6d5be6159440f28d2cb1c6aba
Victor Garcia Zarco <victor.gzarco@gmail.com>
2015-04-10 17:25:35 +0200

    Arreglado el script. Antes, al cargar el formulario con los datos guardados, si había 5 checkbox marcados no desactivaba el resto, y empezaba a contar los 5 a marcar desde cero

--- public/javascripts/max_checkbox_selection.js
+++ public/javascripts/max_checkbox_selection.js
@@ -1,6 +1,16 @@
 function limitCheckboxes(max){
     var maxSelected = max;
     var currentSelected = 0;
+
+    $('.checkbox-group input:checkbox').each(function(){
+        if($(this).prop('checked') == true){
+            currentSelected++;
+        }
+    });
+
+    if(maxSelected == currentSelected){
+        disableOtherChecks();
+    }
     $('.checkbox-group input:checkbox').click(function(){
         if($(this).prop('checked') == true){
             currentSelected++;
@@ -20,6 +30,14 @@ function limitCheckboxes(max){
                             });
         }else{
             currentSelected--;
+        }
+    });
+}
+
+function disableOtherChecks(){
+    $('.checkbox-group input:checkbox').each(function(){
+        if($(this).prop('checked') == false){
+            $(this).prop('disabled', true);
         }
     });
 }
