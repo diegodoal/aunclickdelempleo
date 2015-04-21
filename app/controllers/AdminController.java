7231d0b8af6abf1e82a8fd08aa2dc1e52379ae77
Victor Garcia Zarco <victor.gzarco@gmail.com>
2015-04-21 18:06:20 +0200

    Implementada vista básica de login para administración y comprobación para sitios no autorizados. Falta por añadir más seguridad...

--- app/controllers/AdminController.java
+++ app/controllers/AdminController.java
@@ -3,9 +3,14 @@ package controllers;
 import com.google.gson.Gson;
 import models.datasource.SingletonDataSource;
 import models.entities.User;
+import play.data.DynamicForm;
 import play.mvc.Controller;
 import play.mvc.Result;
+import utils.Utils;
+
 import java.util.List;
+
+import static play.data.Form.form;
 
 
 /**
@@ -13,31 +18,64 @@ import java.util.List;
  */
 public class AdminController extends Controller{
 
-    public static Result blank(String email, String id){
-        List<User> users = SingletonDataSource.getInstance().findAll();
-        if(email == null || id == null)
-            return ok(views.html.admin.admin_main.render(users, null));
+    private final static String ADMIN_USER = "adecco";
+    private final static String ADMIN_PASSWORD = "password";
 
-        for(User user : users){
-            if(user.email.equals(email) && user.id.equals(id)){
-                return ok(views.html.admin.admin_main.render(users, user));
-            }
+    public static boolean checkConnection(String user){
+        if(user != null && user.equals(Utils.encryptWithSHA1(ADMIN_USER))){
+            return true;
+        }else{
+            return false;
         }
+    }
 
-        return ok(views.html.admin.admin_main.render(users, null));
+    public static Result login(){
+        return ok(views.html.admin.login.render());
+    }
+
+    public static Result submitLogin(){
+        DynamicForm bindedForm = form().bindFromRequest();
+
+        String user = bindedForm.get("user");
+        String password = bindedForm.get("password");
+        if(user.equals("adecco") && password.equals("password")){
+            session("user", Utils.encryptWithSHA1(user));
+            return redirect("/admin/show");
+        }
+        return badRequest();
+    }
+
+    public static Result blank(String email, String id){
+        if(checkConnection(session().get("user"))) {
+            List<User> users = SingletonDataSource.getInstance().findAll();
+            if (email == null || id == null)
+                return ok(views.html.admin.admin_main.render(users, null));
+
+            for (User user : users) {
+                if (user.email.equals(email) && user.id.equals(id)) {
+                    return ok(views.html.admin.admin_main.render(users, user));
+                }
+            }
+            return ok(views.html.admin.admin_main.render(users, null));
+        }else{
+            return unauthorized("Access denied");
+        }
     }
 
     public static Result deleteUser(String email, String id){
-        User user = SingletonDataSource.getInstance().getUserByEmail(email);
-        if(user != null && user.id.equals(id)){
-            boolean result = SingletonDataSource.getInstance().deleteUser(email);
-            if(result == true)
-                return redirect("/admin/show");
-            else
-                return badRequest("Cannot delete.");
+        if(checkConnection(session().get("user"))) {
+            User user = SingletonDataSource.getInstance().getUserByEmail(email);
+            if (user != null && user.id.equals(id)) {
+                boolean result = SingletonDataSource.getInstance().deleteUser(email);
+                if (result == true)
+                    return redirect("/admin/show");
+                else
+                    return badRequest("Cannot delete.");
+            } else
+                return badRequest("Cannot delete. User not found or invalid user.");
+        }else{
+            return unauthorized("Access denied");
         }
-        else
-            return badRequest("Cannot delete. User not found or invalid user.");
     }
 
 
